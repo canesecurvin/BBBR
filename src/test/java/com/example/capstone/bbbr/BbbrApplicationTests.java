5bac54626e85703835c12819bf9d62a9cb43847f
Cece Curvin <Canese.Curvin@ibm.com>
2022-10-24 13:14:43 -0500

    Initial Commit

---
+++ src/test/java/com/example/capstone/bbbr/BbbrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.capstone.bbbr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BbbrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
